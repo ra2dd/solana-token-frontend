2ea2128615c04acc1b28afbf1073de8e72865542
ra2dd <radoslawpryw@gmail.com>
2023-11-11 17:17:29 +0100

    add function for creating token account using generated keypair

--- components/CreateTokenAccount.tsx
+++ components/CreateTokenAccount.tsx
@@ -9,13 +9,20 @@ import {
   TOKEN_PROGRAM_ID,
   ASSOCIATED_TOKEN_PROGRAM_ID,
   createAssociatedTokenAccountInstruction,
+  getMint,
+  getAccountLenForMint,
+  createInitializeAccountInstruction,
 } from "@solana/spl-token";
+
+import token from "@solana/spl-token";
 
 export const CreateTokenAccountForm: FC = () => {
   const [txSig, setTxSig] = useState("");
   const [tokenAccount, setTokenAccount] = useState("");
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
+  const [ mint, setMint ] = useState("");
+  const [ accountOwner, setAccoutOwner ] = useState("");
   const link = () => {
     return txSig
       ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet`
@@ -24,11 +31,51 @@ export const CreateTokenAccountForm: FC = () => {
 
   const createTokenAccount = async (event) => {
     event.preventDefault();
-    if (!connection || !publicKey) {
-      return;
+    if (!connection || !publicKey ) {
+      return alert('Connet solana wallet.');
     }
+
+    if (!mint || !accountOwner) {
+      return alert('Make sure mint and account fields are correct.');
+    }
+
     
     // BUILD AND SEND CREATE TOKEN ACCOUNT TRANSACTION HERE
+    // Building transaction account using generated keypair
+    // Certain user can have multiple token accoutns
+    const mintPublicKey = new web3.PublicKey(mint)
+    const mintState =  await getMint(connection, mintPublicKey);
+    const accountKeypair = web3.Keypair.generate();
+    const space = getAccountLenForMint(mintState);
+    const lamports = await connection.getMinimumBalanceForRentExemption(space);
+
+    const transaction = new web3.Transaction().add(
+      web3.SystemProgram.createAccount({
+        fromPubkey: publicKey,
+        newAccountPubkey: accountKeypair.publicKey,
+        space,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeAccountInstruction(
+        accountKeypair.publicKey,
+        mintPublicKey,
+        publicKey,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    try {
+      sendTransaction(transaction, connection, { signers: [accountKeypair] })
+      .then((sig) => {
+        const tokenAccountPublicKeyString = accountKeypair.publicKey.toString();
+        setTokenAccount(tokenAccountPublicKeyString);
+        setTxSig(sig);
+        console.log(sig);
+      });
+    } catch (error) {
+      alert(JSON.stringify(error));
+    }
   };
 
   return (
@@ -43,6 +90,7 @@ export const CreateTokenAccountForm: FC = () => {
             className={styles.formField}
             placeholder="Enter Token Mint"
             required
+            onChange={(event) => setMint(event.target.value)}
           />
           <label htmlFor="owner">Token Account Owner:</label>
           <input
@@ -51,6 +99,7 @@ export const CreateTokenAccountForm: FC = () => {
             className={styles.formField}
             placeholder="Enter Token Account Owner PublicKey"
             required
+            onChange={(event) => setAccoutOwner(event.target.value)}
           />
           <button type="submit" className={styles.formButton}>
             Create Token Account
